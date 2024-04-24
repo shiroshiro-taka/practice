a54b960cf7dd4c9562cb3fef45812c94e1bc35ec
shiroshiro-taka <potetikuitai39@gmail.com>
2024-04-25 07:59:25 +0900

    5回目のコミット

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,24 @@ Route::group(['middleware' => 'auth'], function () {
     Route::resource('products', ProductController::class);
 });
 
+//一覧画面
+Route::get('/index',[ProductController::class, 'index'])->name('product.index');
+
+//詳細画面
+Route::get('/show/{id}', [ProductController::class, 'show'])->name('product.show');
+
+//編集画面
+Route::get('/edit/{id}',[ProductController::class, 'edit'])->name('product.edit');
+
+//新規登録画面
 Route::get('/create', [ProductController::class, 'create'])->name('product.create');
 
+//作成処理
 Route::post('/store', [ProductController::class, 'store'])->name('product.store');
 
-Route::get('/show/{id}', [ProductController::class, 'show'])->name('product.show');
+//編集処理
+Route::post('/update/{id}', [ProductController::class, 'update'])->name('product.update');
+
+//削除処理
+Route::post('/destroy/{id}', [ProductController::class, 'destroy'])->name('product.update');
+
